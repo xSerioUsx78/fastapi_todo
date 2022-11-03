c9c9238dd5026530f562196f5fcecedff9650006
Morteza <lomililmore@gmail.com>
2022-11-03 07:16:23 +0330

    authentication for todos

--- apps/todo_app/views.py
+++ apps/todo_app/views.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from utils.schemas import Page
+from apps.user_app.deps import get_current_user
+from apps.user_app.schemas import UserOut
 from . import schemas
 from . import services
 
@@ -9,20 +11,20 @@ router = APIRouter(
 )
 
 @router.get("/", response_model=Page[schemas.Todo])
-def todo_list():
+def todo_list(_: UserOut = Depends(get_current_user)):
     return services.get_paginated_todo_service()
 
 
 @router.post("/", response_model=schemas.Todo)
-def todo_create(todo: schemas.CreateTodo):
+def todo_create(todo: schemas.CreateTodo, _: UserOut = Depends(get_current_user)):
     return services.create_todo_service(todo)
 
 
 @router.put("/{pk}/", response_model=schemas.Todo)
-def todo_update(todo: schemas.UpdateTodo, pk):
+def todo_update(todo: schemas.UpdateTodo, pk, _: UserOut = Depends(get_current_user)):
     return services.update_todo_service(todo, pk)
 
 
 @router.delete("/{pk}/")
-def todo_delete(pk):
+def todo_delete(pk, _: UserOut = Depends(get_current_user)):
     return services.delete_todo_service(pk)
